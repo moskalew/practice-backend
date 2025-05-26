278286b258bb01c8ab7c6ebfa406805f84b22b07
Andrei <moskalev.web.dev@gmail.com>
2025-05-26 22:19:18 +0300

    5. Переключаем приложение на работу с MongoDB

--- 4. Web-сервер/notes.controller.js
+++ 4. Web-сервер/notes.controller.js
@@ -1,49 +1,30 @@
-const fs = require('fs/promises')
-const path = require('path')
-const chalk = require('chalk')
-
-const notesPath = path.join(__dirname, 'db.json')
+const chalk = require('chalk');
+const Note = require('./models/Note');
 
 async function addNote(title) {
-  const notes = await getNotes()
-  const note = {
-    title,
-    id: Date.now().toString()
-  }
+  await Note.create({ title });
 
-  notes.push(note)
-
-  await saveNotes(notes)
-  console.log(chalk.bgGreen('Note was added!'))
+  console.log(chalk.bgGreen('Note was added!'));
 }
 
 async function getNotes() {
-  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
-  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
-}
-
-async function saveNotes(notes) {
-  await fs.writeFile(notesPath, JSON.stringify(notes))
-}
-
-async function printNotes() {
-  const notes = await getNotes()
-
-  console.log(chalk.bgBlue('Here is the list of notes:'))
-  notes.forEach(note => {
-    console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
-  })
+  const notes = await Note.find();
+  return notes;
 }
 
 async function removeNote(id) {
-  const notes = await getNotes()
+  await Note.deleteOne({ _id: id });
+  console.log(chalk.red(`Note with id="${id}" has been removed.`));
+}
 
-  const filtered = notes.filter(note => note.id !== id)
-
-  await saveNotes(filtered)
-  console.log(chalk.red(`Note with id="${id}" has been removed.`))
+async function updateNote(noteData) {
+  await Note.updateOne({ _id: noteData.id }, { title: noteData.title });
+  console.log(chalk.bgGreen(`Note with id="${noteData.id}" has been updated!`));
 }
 
 module.exports = {
-  addNote, getNotes, removeNote
-}
+  addNote,
+  getNotes,
+  removeNote,
+  updateNote,
+};
